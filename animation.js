fa997d8c8d23d1d78c2def6261ebb34bf588265f
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-22 14:30:58 +0530

    simple header animation

---
+++ animation.js
@@ -0,0 +1,12 @@
+gsap.from("#mehefuj-hero", {
+
+      x: 100,
+      duration: 2,
+      opacity: 0
+})
+gsap.from(".hero-texts", {
+      opacity: 0,
+      x: -20,
+      duration: 2,
+      stagger: .4
+})
