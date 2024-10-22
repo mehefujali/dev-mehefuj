ac9127599954a8bc00e27d02e87f93d122ecde13
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-22 14:57:45 +0530

    animation added

--- animation.js
+++ animation.js
@@ -10,3 +10,39 @@ gsap.from(".hero-texts", {
       duration: 2,
       stagger: .4
 })
+gsap.from("#ab-ani-img", {
+      opacity: 0,
+      x: -200,
+      duration: 2,
+      stagger: .4,
+      scrollTrigger: {
+            trigger: "#ab-ani-img",
+            // markers: true,
+            start: "top 60%"
+
+      }
+})
+gsap.from("#ab-ani-txt", {
+      opacity: 0,
+      x: 150,
+      duration: 2,
+
+      scrollTrigger: {
+            trigger: "#ab-ani-txt",
+            start: "top 60%"
+
+      }
+})
+gsap.from(".skill-card", {
+      opacity: 0,
+      x: 150,
+      duration: 2,
+      stagger: .4,
+      scrollTrigger: {
+            trigger: ".skill-card",
+            start: "top 60%"
+
+      }
+})
+
+
